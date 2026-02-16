e26d9e1763fae29ab99377482f6a3611d4f9affd
chukwuebuka <okolisamuel21@gmail.com>
2026-02-16 13:39:16 +0100

    made a change

--- name.js
+++ name.js
@@ -10,3 +10,9 @@ console.log(randIndex);
 console.log(arrayOfNumbers[randIndex]);
 
 hvshsh;
+“git log --pretty=format:"%h %s" --graph”
+
+Excerpt From
+Pro Git
+Scott Chacon
+This material may be protected by copyright.
